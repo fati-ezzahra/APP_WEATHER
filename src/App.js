1adb76c9fb4d821e5db8d281b5eb4cfc90e1331e
EL AYOUCHI FATIMA-EZZAHRA <elayouchi.fati.ezzahra@gmail.com>
2021-10-31 14:39:48 +0100

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component{
     const city=e.target.elements.city.value;
     const country=e.target.elements.country.value;
     e.preventDefault();
-    const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${API_KEY}`)
+    const api_call=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${API_KEY}`)
     const response=await api_call.json()
     if(city && country){
       this.setState({
